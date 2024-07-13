749e8a5e1dede6b904ee67bb19c651cde3c2a96f
Rustem Kakimov <ruskakimov@gmail.com>
2024-07-13 19:47:31 -0400

    Idol data

--- app/src/main/java/com/example/kpop/idol/catalog/MainActivity.kt
+++ app/src/main/java/com/example/kpop/idol/catalog/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kpop.idol.catalog
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
@@ -20,14 +22,58 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Populate KPOP group spinner with the list of groups.
-        val spinner: Spinner = findViewById(R.id.kpop_groups_spinner)
+        val groupSpinner: Spinner = findViewById(R.id.kpop_groups_spinner)
+
         ArrayAdapter.createFromResource(
             this,
             R.array.groups_array,
             android.R.layout.simple_spinner_item
         ).also { adapter: ArrayAdapter<CharSequence> ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = adapter
+            groupSpinner.adapter = adapter
+        }
+
+        groupSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedGroup = parent.getItemAtPosition(position).toString()
+                println(selectedGroup)
+                val idols = getIdols(selectedGroup)
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+    }
+
+    private fun getIdols(groupName: String): List<Idol> {
+        return when (groupName) {
+            "Twice" -> listOf(
+                Idol("Nayeon", "임나연", "September 22, 1995", "ISTP"),
+                Idol("Jeongyeon", "유정연", "November 1, 1996", "ISFJ"),
+                Idol("Momo", "모모", "November 9, 1996", "INFP"),
+                Idol("Sana", "사나", "December 29, 1996", "ENFP"),
+                Idol("Jihyo", "박지효", "February 1, 1997", "ISFP"),
+                Idol("Mina", "미나", "March 24, 1997", "ISFP"),
+                Idol("Dahyun", "다현", "May 28, 1998", "ISFJ"),
+                Idol("Chaeyoung", "채영", "April 23, 1999", "INFP"),
+                Idol("Tzuyu", "쯔위", "June 14, 1999", "ISFP")
+            )
+            "ITZY" -> listOf(
+                Idol("Yeji", "예지", "May 26, 2000", "ESFJ"),
+                Idol("Lia", "리아", "July 21, 2000", "ENFJ"),
+                Idol("Ryujin", "류진", "April 17, 2001", "INTJ"),
+                Idol("Chaeryeong", "채령", "June 5, 2001", "INFJ"),
+                Idol("Yuna", "유나", "December 9, 2003", "ENFP")
+            )
+            "Blackpink" -> listOf(
+                Idol("Jisoo", "지수", "January 3, 1995", "INFJ"),
+                Idol("Jennie", "제니", "January 16, 1996", "ISTP"),
+                Idol("Rosé", "로제", "February 11, 1997", "ISFP"),
+                Idol("Lisa", "리사", "March 27, 1997", "ENFP")
+            )
+            else -> listOf()
         }
     }
 }
+
+data class Idol(val name: String, val koreanName: String, val birthday: String, val mbti: String)
